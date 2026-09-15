2aeb98dd89d9e80e35b00d746be6c9e9425baf7e
Jagadeesh B <bjagadeesh838@gmail.com>
2026-09-15 22:17:25 +0530

    fire base added

--- peppa-netflix/src/utils/firebase.js
+++ peppa-netflix/src/utils/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAkFRTnd0W5WXShEZiPByMbwzWrls2Qs18",
-  authDomain: "jaga-netflix.firebaseapp.com",
-  projectId: "jaga-netflix",
-  storageBucket: "jaga-netflix.firebasestorage.app",
-  messagingSenderId: "141863214538",
-  appId: "1:141863214538:web:8a751ff57d2a394c7d8333",
-  measurementId: "G-R3QZVKTT1R"
+  apiKey: "AIzaSyAm6hRitCdCdlPr6rZYrkoh7QnnOcWHQEc",
+  authDomain: "jaga-netflix-fresh.firebaseapp.com",
+  projectId: "jaga-netflix-fresh",
+  storageBucket: "jaga-netflix-fresh.firebasestorage.app",
+  messagingSenderId: "358821412466",
+  appId: "1:358821412466:web:ac0bf56a4c668002c24ceb",
+  measurementId: "G-PKP9YX8GT5"
 };
 
 // Initialize Firebase
