3a399d37f73cf269ab9e3bc6ccd6ecbc114909af
Jagadeesh B <bjagadeesh838@gmail.com>
2026-09-19 17:43:14 +0530

    api integration for top movies

---
+++ peppa-netflix/src/components/MoviesList.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import MovieCard from './MovieCard'
+
+const MoviesList = ({ title, movies }) => {
+  console.log("ln5--->", movies);
+  return (
+    <div className="px-6 bg-black text-white">
+      <h1 className="text-3xl py-4">{title}</h1>
+      <div className="flex overflow-x-scroll">
+        <div className="flex">
+            {movies?.map(movie => <MovieCard key={movie?.id} posterPath={movie?.poster_path} />)}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MoviesList
--- peppa-netflix/src/components/SecondaryContainer.js
+++ peppa-netflix/src/components/SecondaryContainer.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import MoviesList from './MoviesList'
+import { useSelector } from 'react-redux'
 
 const SecondaryContainer = () => {
+  const movies = useSelector(store => store.movies);
   return (
-    <div></div>
+    <div className="bg-black">
+      <div className="-mt-52 relative pl-12 z-20">
+     <MoviesList title={"Now Playing"} movies={movies.nowPlayingMovies}/>
+      <MoviesList title={"Trending"} movies={movies.nowPlayingMovies}/>
+      
+      <MoviesList title={"Popular"} movies={movies.nowPlayingMovies}/>
+      <MoviesList title={"Upcoming Movies"} movies={movies.nowPlayingMovies}/>
+      <MoviesList title={"Horror"} movies={movies.nowPlayingMovies}/>
+      </div>
+     
+    </div>
   )
 }
 
